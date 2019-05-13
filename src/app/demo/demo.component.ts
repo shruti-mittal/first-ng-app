035a24be1465b1e1d6fd9de64cb06c0350d09f15
Shruti Mittal <shrutimittal0404@gmail.com>
2019-05-13 16:53:11 +0530

    [Class Binding] : binding classes to html elements

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -3,20 +3,30 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-demo',
   template: `
-    <div>
+    <div [ngClass]="greetClasses">
       Hi {{user.toUpperCase() + '!'}} at {{getSiteUrl()}}
     </div>
     <div>
-      <input [id]="bindID" type="text" value="username" bind-disabled="!isDisabledProperty">
-      <input id="{{bindID}}" type="button" [disabled]="isDisabledProperty" value="Ok">
+      <input [id]="bindID" class="is-guest" bind-disabled="!isDisabledProperty" type="text" value="username">
+      <input id="{{bindID}}" class="bold-admin" [class]="userType" [disabled]="isDisabledProperty" type="button" value="Ok">
     </div>
-    <p>
+    <p [class.bold-admin]="!hasAdminRights">
       'demo' as sub-component (with inline template) works!
     </p>
   `,
   styles: [`
     p {
       color: aquamarine;
+    }
+    .is-admin {
+    color: green;
+    }
+    .is-guest {
+    color: blue;
+    }
+    .bold-admin {
+    font-weight: bolder;
+    background-color: lightgrey;
     }
   `]
 })
@@ -26,6 +36,14 @@ export class DemoComponent implements OnInit {
 
   public bindID = 'demoID';   // {{Interpolation}}, [Property Binding] - for string values
   public isDisabledProperty = true;   // [Property Binding] - (for non-string data values) : binding to the 'id' property of DOM element (dom property), not to html attribute
+
+  public userType = 'is-admin';    // [Class Binding] : dynamically binds (add/remove) class(es) to html elements
+  public hasAdminRights = true;   // property to conditionally apply single class based on expression that evaluates to be either true or false.
+  public greetClasses = {
+    "is-admin": this.hasAdminRights,
+    "is-guest": !this.hasAdminRights,
+    "bold-admin": this.hasAdminRights
+  };   // object to conditionally apply multiple classes, Use : ngClass Directive
 
   constructor() {
   }
