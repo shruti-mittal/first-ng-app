9d7b8b31194b1935d46c51dae93f1f1005703278
Shruti Mittal <shrutimittal0404@gmail.com>
2019-05-14 12:40:13 +0530

    (Event Binding) : to capture dom events and perform some action

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -7,8 +7,11 @@ import {Component, OnInit} from '@angular/core';
       Hi {{user.toUpperCase() + '!'}} at {{getSiteUrl()}}
     </div>
     <p>
-      <input [id]="bindID" class="is-guest" bind-disabled="isDisabledProperty" type="text" value="username">
-      <input (click)="getUA()" id="{{bindID}}" class="bold-admin" [class]="userType" [disabled]="!isDisabledProperty" type="button" value="Ok">
+      <input [id]="bindID" class="is-guest" bind-disabled="isDisabledProperty"
+             type="text" value="username">
+      <input (click)="getUA($event)" id="{{bindID}}" class="bold-admin" [class]="userType"
+             [disabled]="!isDisabledProperty"
+             type="button" value="Ok">
     </p>
     <p [class.bold-admin]="!hasAdminRights">
       'demo' as sub-component (with inline template) works!
@@ -21,26 +24,33 @@ import {Component, OnInit} from '@angular/core';
         Knowing : [Event binding] currently.
       </p>
     </div>
+    <div>
+      <input type="text" placeholder="Email for our newsletter">
+      <button (click)="user = 'Editor'; hasAdminRights = true;">Subscribe</button>
+    </div>
   `,
   styles: [`
     p {
       color: aquamarine;
     }
+
     .is-admin {
-    color: green;
+      color: green;
     }
+
     .is-guest {
-    color: blue;
+      color: blue;
     }
+
     .bold-admin {
-    font-weight: bolder;
-    background-color: lightgrey;
+      font-weight: bolder;
+      background-color: lightgrey;
     }
   `]
 })
 export class DemoComponent implements OnInit {
 
-  public user = 'Default User';  // {{Interpolation}} - (embedding expressions) : binds 'string' data from the class to the template
+  public user = 'Admin';  // {{Interpolation}} - (embedding expressions) : binds 'string' data from the class to the template
 
   public bindID = 'demoID';   // {{Interpolation}}, [Property Binding] - for string values
   public isDisabledProperty = true;   // [Property Binding] - (for non-string data values) : binding to the 'id' property of DOM element (dom property), not to html attribute
@@ -60,8 +70,8 @@ export class DemoComponent implements OnInit {
     fontStyle: "normal"
   };    // object to apply Multiple Styles, Use : ngStyle Directive
 
-  // Event Binding : to respond to/capture user events, we need data flow in other direction as well (from template/view to component-class)
-
+  // -- [Data Binding] : Data flow is from component class to component template. Any update to class property makes view updated as well --
+  // -- (Event Binding) : to respond to/capture user(dom) events, we need data flow in other direction as well (from template/view to component-class) --  //2 ways to use : assign Event handler | add Template statement inline
 
   constructor() {
   }
@@ -69,12 +79,17 @@ export class DemoComponent implements OnInit {
   ngOnInit() {
   }
 
-  getSiteUrl() {
+  getSiteUrl() {    // {{Interpolation}}
     return window.location.href;
   }
 
-  getUA() {
-    alert('UA: ' + navigator.userAgent);
+  getUA(event) {   // (Event Binding) - event handler to listen to click event of button 'Ok'
+    // setting properties on dom event -  mouse click!
+    this.user = 'Guest User';
+    this.hasAdminRights = false;
+
+    console.log('Event is - ', event);    //$event - (optional event handler parameter) - special variable for angular - gives all info about dom event that was raised.
+    alert('This User Agent : ' + navigator.userAgent);
   }
 
 }
