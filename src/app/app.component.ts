397334c65b9ff1a1d485254f5b3f206bc23d0967
Shruti Mittal <shrutimittal0404@gmail.com>
2019-05-15 19:33:39 +0530

    Basic Calculator : Allow only valid arithmetic calc keys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,11 @@ export class AppComponent {
   title = 'Perform Calcs.';
 
   public expression: any = '';
-  public result;
+  public result: number;
 
-  evaluate() {
-    console.log('exp. is becoming=', this.expression);
+  evaluate(event) {
+    event.target.value = event.target.value.toString().replace(/[^\d\+\-\*\/\%\(\)]/gi,'');
+    //console.log(0o2+4);   //to allow octal literal in strict mode!
     this.result = eval(this.expression);
   }
 
