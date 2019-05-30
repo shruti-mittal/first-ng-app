09d4def55365f9f08443c91837c8d5be57d5d2eb
Shruti Mittal <shrutimittal0404@gmail.com>
2019-05-30 12:15:55 +0530

    Store todos : Feature to persist data locally using localStorage (web api) even when tab is closed.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -16,19 +16,38 @@ export class TodoListComponent implements OnInit, OnChanges {
 
   // OnChanges() method hook gets triggered whenever ng detects changes to input properties of the component (or directive).
   ngOnChanges(changes: SimpleChanges) {
-    if (changes.todoAdded.currentValue)
+    if (changes.todoAdded.currentValue) {
       this.taskList.push(changes.todoAdded.currentValue);
+      this.saveInLocalStorage();
+    }
   }
 
+  // OnInit() method hook runs only once when this angular component gets initialized
   ngOnInit() {
+    // LocalStorage: Keeps data locally in browser's local storage. Data with no expiration date that will persist after the browser window is closed (until the user manually clears the browser cache or until your web app clears the data).
+    // SessionStorage: Keeps data locally in browser's session storage. Data gets cleared after the browser window is closed (from all tabs opened for this domain).
+    // These are synchronous HTML5 web Storage API {For async process, use IndexedDB web storage API}. These data cannot be read by server. Hence safe (unlike cookies). Every domain has its data storage given.
+    // These APIs have their events also to put some more features & error emitting support to detect if max storage size(10MB mostly) is reached for this domain.
+
+    this.getFromLocalStorage();
   }
 
   deleteTodo(index) {
     this.taskList.splice(index, 1);
+    this.saveInLocalStorage();
   }
 
   toggleTodo(todo) {
     todo.completed = !todo.completed;
+    this.saveInLocalStorage();
+  }
+
+  getFromLocalStorage() {
+    this.taskList = JSON.parse(localStorage.getItem('taskList')) || [];   //similarly for Session Storage : sessionStorage.getItem('taskList')
+  }
+
+  saveInLocalStorage() {
+    localStorage.setItem('taskList', JSON.stringify(this.taskList));   //similarly for Session Storage : sessionStorage.setItem('taskList', stringValue)
   }
 
 }
